ab7a7766aa7f898685dbfc0dcc437fd7cb03457f
Jacreator <jambone.james82@gmail.com>
2021-04-16 18:17:01 +0100

    init commit

---
+++ Vue_app/src/views/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home col-8 mx-auto py-5 mt-5">
+    <h1>Dashboard</h1>
+    <div class="card">
+      <div class="card-body">
+        <h3>Hello, World</h3>
+        <!-- <span>{{ user.email }}</span> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import User from "../apis/User";
+export default {
+  // data() {
+  //   return {
+  //     user: null
+  //   };
+  // },
+  // mounted() {
+  //   User.auth().then(response => {
+  //     this.user = response.data;
+  //   });
+  // }
+};
+</script>
